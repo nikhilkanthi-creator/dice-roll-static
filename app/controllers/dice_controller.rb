4206c1b658d802a026cc10dd33ae30681ad9cb22
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-23 01:28:32 +0000

    first one working!

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,5 +1,11 @@
 class DiceController < ApplicationController
+  def home
+   
+    render({ :template => "dice_templates/home"})
+  end
   def two_six
+    @dice1 = rand(1..6)
+    @dice2 = rand(1..6)
     render({ :template => "dice_templates/2d6"})
   end
 end
